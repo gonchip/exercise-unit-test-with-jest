c8efe0c7cc9e4f17e1a99fe0eb5ea2eea97cc11c
gonchip <136641475+gonchip@users.noreply.github.com>
2023-09-18 22:07:20 +0000

    tests created

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
-const sum = (a,b) => {
+const sum = (a, b) => {
     return a + b
 }
 
-console.log(sum(7,3))
+console.log(sum(7, 3))
 
 module.exports = { sum };
 
 
-const fromEuroToDollar = function(valueInEuro){
-    
+const fromEuroToDollar = function (valueInEuro) {
+
     let valueInDollar = valueInEuro * 1.2;
-    
+
     return valueInDollar;
 }
 
-module.exports = { sum, fromEuroToDollar }
+
+const fromDollarToYen = function (valueInDollar) {
+
+    let valueInYen = valueInDollar * 106.58;
+
+    return valueInYen;
+}
+
+
+const fromYenToPound = function (valueInYen) {
+
+    let valueInPound = valueInYen * 0.006251;
+
+    return valueInPound;
+}
+
+
+module.exports = {
+    sum, fromEuroToDollar,
+    fromDollarToYen,
+    fromYenToPound
+}
--- test.js
+++ test.js
@@ -7,10 +7,35 @@ test('adds 14 + 9 to equal 23', () => {
     expect(total).toBe(23);
 });
 
-test("One euro should be 1.206 dollars", function(){
+test("One euro should be 1. dollars", function(){
 
     const { fromEuroToDollar } = require('./app.js')
     
     
     expect(fromEuroToDollar(3.5)).toBe(4.2); 
+})
+
+
+
+const { fromDollarToYen } = require('./app');
+
+
+test("One dollar should be 106.58 yen", function(){
+    
+    const result = fromDollarToYen(1);
+    
+    
+    expect(result).toBe(106.58); 
+})
+
+
+const { fromYenToPound } = require('./app');
+
+
+test("One yen should be 0.006251 pounds", function(){
+    
+    const result = fromYenToPound(1);
+    
+    
+    expect(result).toBe(0.006251); 
 })
